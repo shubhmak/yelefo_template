6780141cf311013332893de2e61e50bc0936ae32
shubhmak <shumak1144@gmail.com>
2021-07-05 09:24:15 +0100

    initial commit

---
+++ yelefo_animated/assets/js/typer.js
@@ -0,0 +1,10 @@
+$('document').ready(function(){
+    var typed = new Typed('#typed',{
+                    strings:["<strong>Distributor</strong> searching for <strong>Manufacturer</strong> ?","<strong>Manufacturer</strong> searching for <strong>Distributor</strong>  ?"],
+                    backSpeed: 40,
+                    typeSpeed: 40,
+                    loop: true,
+                    loopCount: Infinity
+                  });
+    
+});
